a4ce3d820d6f9e8fdff00122bd41eaf5df35334c
Nemanja <91620216+nebocoder@users.noreply.github.com>
2022-10-21 18:20:16 +0200

    Change navbar bg color to red

--- src/modules/layout/templates/nav/index.tsx
+++ src/modules/layout/templates/nav/index.tsx
@@ -45,20 +45,10 @@ const Nav = () => {
         "!fixed": isHome,
       })}
     >
-      <header
-        className={clsx(
-          "relative h-16 px-8 mx-auto transition-colors bg-transparent border-b border-transparent duration-200 group-hover:bg-white group-hover:border-gray-200",
-          {
-            "!bg-white !border-gray-200": !isHome || isScrolled,
-          }
-        )}
-      >
+      <header className="relative h-16 px-8 mx-auto text-white bg-red-700 border-b border-red-900">
         <nav
           className={clsx(
-            "text-gray-900 flex items-center justify-between w-full h-full text-small-regular transition-colors duration-200",
-            {
-              "text-white group-hover:text-gray-900": isHome && !isScrolled,
-            }
+            "text-white flex items-center justify-between w-full h-full text-small-regular transition-colors duration-200"
           )}
         >
           <div className="flex-1 basis-0 h-full flex items-center">

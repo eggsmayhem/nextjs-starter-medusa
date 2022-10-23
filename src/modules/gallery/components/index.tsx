a1bcd815755c6b475b3b680c5724ce5b1d1462e4
Ryan Oh <ryanohnoes@gmail.com>
2022-10-23 11:32:44 -0700

    updated alt tags for commissions page

--- src/modules/gallery/components/index.tsx
+++ src/modules/gallery/components/index.tsx
@@ -3,20 +3,23 @@ interface FormatGalleryProps {
   url1: string
   url2: string
   url3: string
+  des1: string
+  des2: string
+  des3: string
 }
-const Gallery = ({ category, url1, url2, url3 }: FormatGalleryProps) => {
+const Gallery = ({ category, url1, url2, url3, des1, des2, des3 }: FormatGalleryProps) => {
   return (
     <>
       <h1 className="text-center text-3xl font-bold">{category}</h1>
       <div className="container pt-12 pb-24 justify-items-center place-items-start mx-auto lg:gap-8 lg:grid lg:grid-cols-3">
         <div className="w-full aspect-square rounded hover:opacity-75 hover:shadow-xl">
-          <img src={url1} alt="image" className="w-full h-full object-cover" />
+          <img src={url1} alt={des1} className="w-full h-full object-cover" />
         </div>
         <div className="w-full aspect-square rounded hover:opacity-75 hover:shadow-xl">
-          <img src={url2} alt="image" className="w-full h-full object-cover" />
+          <img src={url2} alt={des2} className="w-full h-full object-cover" />
         </div>
         <div className="w-full aspect-square rounded hover:opacity-75 hover:shadow-xl">
-          <img src={url3} alt="image" className="w-full h-full object-cover" />
+          <img src={url3} alt={des3} className="w-full h-full object-cover" />
         </div>
       </div>
     </>
--- src/pages/commissions.tsx
+++ src/pages/commissions.tsx
@@ -14,30 +14,45 @@ const Commissions: NextPageWithLayout = () => {
             url1="/six.jpg"
             url2="/pinkcat.jpg"
             url3="Unholey.jpg"
+            des1="an interpretation of the world on fire from angels judgment"
+            des2="a watermelon theme of a playful pink panther on a background of green and black."
+            des3="a block of holey cheese if were poked with a searing hot staff"
           />
           <Gallery
             category="Tattoos"
             url1="/crow.jpg"
             url2="/plank.jpg"
             url3="/love.jpg"
+            des1="A black drunk bird dropping its bottle"
+            des2="A tattoo of Plank from Ed, Edd, and Eddy"
+            des3="A tattoo of the text 'Love After Pain' writen in olde english"
           />
           <Gallery
             category="Decor"
             url1="/shit.jpg"
             url2="/eyetable.jpg"
             url3="/butterfly.jpg"
+            des1="A glow in the dark Toilet Paper Bathroom Sign that reads 'Shit tickets' in old english"
+            des2="a three-tier eye ball table. The top tier has a bloodshot blue eye with blood hand prints on the corners. The second tier has a red border with a black center. The bottom tier is blue"
+            des3="Light blue and purple wall mounted buttterfly supports that can handle light weight items"
           />
           <Gallery
             category="Accessories"
             url1="/stickergroup.jpg"
             url2="/tittypin.jpg"
             url3="/eyeballpin.jpg"
+            des1="a collection of psychotic and weird stickers"
+            des2="a pin that can play an illusion with your eyes with ducks and legs"
+            des3="a dead three blue-eyed eyeball with a halo is extending its tentacales with thorns"
           />
           <Gallery
             category="Apparel"
             url1="/killgodsweater.jpg"
             url2="/ratvestback.jpg"
             url3="/ratvestfront.jpg"
+            des1="a sweater with the text that says 'KILL GOD' in olde english text"
+            des2="a psychotic, red and yellow eyed, green rat with blood covered demonic teeth holding a block of cheese on a denim vest"
+            des3="metallic sickles with wooden handles on a teal background on a denim vest"
           />
         </div>
       </div>

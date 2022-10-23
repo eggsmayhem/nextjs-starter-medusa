d40611dc0158b682746f215859a71c6685048128
Sabrina Pyles <flipmybit@protonmail.com>
2022-10-23 11:29:51 -0400

    styled form

--- src/modules/layout/components/commissions-form/index.tsx
+++ src/modules/layout/components/commissions-form/index.tsx
@@ -27,15 +27,16 @@ const CommissionsForm = () => {
       };
 
     return (
-        <div>
-      <h1 className="text-center text-md-left mb-3">Get in Touch / Request a Commission</h1>
-      <form className="contact-form" onSubmit={handleSubmit(sendEmail)}>
-        <div className="form-group mb-0 py-3">
+      <div className='flex flex-col items-center'>
+      <h1 className="text-center text-md-left mb-3 justify-self-center text-3xl">Get in Touch / Request a Commission</h1>
+      <form className="contact-form self-center justify-self-center w-96" onSubmit={handleSubmit(sendEmail)}>
+        <div className="form-group mb-0 py-3 self-center justify-self-center">
           <textarea
-            className="form-control custom--fields-mod text-the-primary"
+            // className="form-control custom--fields-mod text-the-primary self-center justify-self-center"
+            className="form-control custom--fields-mod text-the-primary block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             id="message"
             rows={3}
-            placeholder="Message *"
+            placeholder="What is your vision?"
             name="message"
             {...register("message", { required: true })}
           ></textarea>
@@ -48,19 +49,6 @@ const CommissionsForm = () => {
         <div className="form-group row py-2 mb-0">
           <div className="col-md-6">
             <div>
-              <div className="d-flex align-items-center">
-                <input
-                  className="mr-2"
-                  type="checkbox"
-                  id="yes_i_understand"
-                  name="yes_i_understand"
-                  {...register("yes_i_understand", { required: true })}
-                />
-                <label className="font-size-12 mb-0" htmlFor="yes_i_understand">
-                  I understand and agree to the Privacy Policy and Terms and
-                  Conditions.
-                </label>
-              </div>
               {errors.yes_i_understand && (
                 <div
                   className="invalid-feedback d-block"
@@ -73,7 +61,7 @@ const CommissionsForm = () => {
           </div>
           <div className="col-md-6 text-center text-md-left py-2 py-md-0">
             <input
-              className="buttons-width float-md-right btn btn-dark-moderate-orange rounded-0"
+              className="buttons-width float-md-right btn btn-dark-moderate-orange rounded-md bg-red-700 p-2 text-white"
               type="submit"
               value="SEND MESSAGE"
             />

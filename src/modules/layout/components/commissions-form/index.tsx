2872bf8c8e5421f03e6c95fc093e8d5760ea4d3a
Sabrina Pyles <flipmybit@protonmail.com>
2022-10-23 13:21:31 -0400

    commissions form styling

--- src/modules/layout/components/commissions-form/index.tsx
+++ src/modules/layout/components/commissions-form/index.tsx
@@ -6,8 +6,9 @@ import { send } from 'emailjs-com'
 const CommissionsForm = () => {
     return (
       <div className='flex flex-col items-center bg-black p-4'>
-        <h1 className="text-3xl text-white text-center m-4">Commission custom art, apparrel, decor, accessory, or tattoo</h1>
-        <h3 className="text-white text-center m-2">Introduce yourself and share your vision! The artist will contact you about how to manifest the exact piece you want, and to determine a fair price</h3>
+        <h1 className="text-3xl text-white text-center m-4 font-bold">Commission custom art</h1>
+        <h3 className="text-white text-center m-1">Introduce yourself and share your vision!</h3>
+        <h3 className="text-white text-center">The artist will contact you about how to manifest the exact piece you want, and to determine a fair price</h3>
         <a
           href="mailto:eyeball4800@gmail.com"
           className="rounded-md bg-red-700 p-2 text-white m-5"
